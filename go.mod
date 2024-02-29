1ae10e857caacb6871e776313a760289a471228c
Juan A. Garcia Pardo <juagargi@gmail.com>
2024-02-29 15:07:31 +0100

    Standalone handles path types.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/juagargi/plugin-standalone
+
+go 1.21.6
+
+require github.com/juagargi/plugin-test v0.0.0-20240229140320-94f481930f5e // indirect
--- standalone.go
+++ standalone.go
@@ -1,9 +1,34 @@
 package main
 
-func OneFunction() string {
-	return "standalone: OneFunction"
+import (
+	"fmt"
+
+	"github.com/juagargi/plugin-test/pkg/snet"
+)
+
+// Declare declares that this is a path plugin.
+func Declare() string {
+	return "path"
 }
 
-func Declare() string {
-	return "standalone declares: " + OneFunction()
+type Main struct{}
+
+func (Main) Doc() string {
+	return `
+	This is the documentation of the standalone path handler plugin.
+	`
+}
+
+func (Main) PathType() string {
+	return "epic"
+}
+
+func (Main) Handler() snet.PacketHandlerFunc {
+	return Handle
+}
+
+func Handle(packet *snet.Packet) error {
+	fmt.Printf("colibri handling %d sized packet\n", packet.SizeOfPacket)
+	packet.CurrentHop++
+	return nil
 }
